734eec1df677a8d41968aa02476b32e37acfb73c
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-11-29 15:08:59 +0200

    update google fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {ThemeProvider} from 'src/context/theme-context';
+import { Helmet } from "react-helmet";
 
 export const wrapRootElement = ({element}) => (
   <ThemeProvider>
+    <Helmet>
+      <link rel="preload" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" />
+    </Helmet>
     {element}
   </ThemeProvider>
 );
