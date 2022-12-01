9a63eff39638032aab85f0b0c77dad3231856dc8
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-01 13:36:15 +0200

    update state

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,11 +96,6 @@ module.exports = {
         schema: {
           requestConcurrency: 1,
         },
-        type: {
-          __all: {
-            limit: 30
-          },
-        },
       }
     },
     {
