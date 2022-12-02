d48bfbeaecba23acb4046a0833f91fbe57270bb7
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-02 15:34:32 +0200

    update preact

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {ThemeProvider} from 'src/context/theme-context';
+import { Helmet } from "react-helmet";
 
 export const wrapRootElement = ({element}) => (
   <ThemeProvider>
+    <Helmet>
+        <link rel="preconnect" href="https://fonts.gstatic.com/"  />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Merriweather:wght@400;700&display=swap" rel="stylesheet"/>
+    </Helmet>
     {element}
   </ThemeProvider>
 );
