57783c582e9934de4722041a7e672cde45d732e6
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-03 17:16:03 +0200

    update preact

--- src/components/posts/featured-image.jsx
+++ src/components/posts/featured-image.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import {GatsbyImage, getImage} from "gatsby-plugin-image";
-
+import {GatsbyImage, getImage } from "gatsby-plugin-image";
 const typePropsImage = (image) => typeof image === "object" && image !== null
 const FeaturedImage = ({image, lazy = false}) => {
-    console.log(image, lazy)
-    if (!typePropsImage(image)) return null;
+    console.log(getImage(image))
+    if(!typePropsImage(image)) return null;
     return (
         <>
             {lazy ?
                 <div className="gatsby-image-wrapper gatsby-image-wrapper-constrained team_block">
-                    <img src={image.childImageSharp.fluid?.base64} alt="Red dot"/></div>
+                    <img src={getImage(image).images.fallback.src} alt=""/>
+                </div>
                 :
                 <GatsbyImage
                     imgClassName="team_img"
                     className="team_block"
-                    urationFadeIn={0}
                     image={getImage(image)}
                     alt="123"
-                />}
+                />
+            }
         </>
     )
 }
