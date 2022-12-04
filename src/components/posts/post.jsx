3bae81c06cd0d46e1ece9862791befe4a59f9045
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-04 14:49:23 +0200

    update preact

--- src/components/posts/post.jsx
+++ src/components/posts/post.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import FeaturedImage from "./featured-image";
-import {Link} from "gatsby";
 
 const Post = ({title, featuredImage, uri, index, categories}) => {
     return (
 
         <div className="item">
             <a href={uri} className="img">
-                <FeaturedImage image={featuredImage} lazy={index > 0}/>
+                <FeaturedImage image={featuredImage} lazy={index > 2}/>
             </a>
             <div className="txt">
                 <div className="post-category">
                     {categories.nodes.slice(0, 4).map((cat, index, array) => (
                         <a href={cat.uri} key={cat.id} >{cat.name} {index < array.length-1 ? ',' : null}</a>
                     ))}
-                    <h3><Link to={uri} >{title}</Link></h3>
+                    <h3><a href={uri} >{title}</a></h3>
                     <div className="excerpt">
                         If you wonder what bed bugs are, how do they spread, just how fast they can infest your home and
                         what you can do about it watch this quick video: If you...
