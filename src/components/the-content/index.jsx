a6e531e6ead4395ed192c3dcd97cc202a5b38a4c
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-04 13:47:01 +0200

    update preact

--- src/components/the-content/index.jsx
+++ src/components/the-content/index.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import parse from "html-react-parser";
 import './style.css';
+import {youtubeParser} from "../../util/helpers";
+import YouTubeLazy from "../youtube";
 
 const TheContent = ({text, title}) => {
     if(!text) return null;
     return(
         <div className="the-content">
             <h2>{title}</h2>
-             {parse(text)}
+             {parse(text, {
+                 replace: domNode => {
+                     if (domNode.name === 'iframe' && youtubeParser(domNode.attribs.src)) {
+                         console.log(domNode)
+                         return (
+                             <YouTubeLazy
+                                 videoId={youtubeParser(domNode.attribs.src)}
+                                 style={{height: domNode.attribs.height+'px', width: domNode.attribs.width+'px'}}
+                             />
+                         )
+                     }
+                 }
+             })}
         </div>
     )
 }
