37b54d74e54b0d0e3c56abc25be30754ec32da45
Ihor Krukovskyi <ihor.krukovskyi@gmail.com>
2022-12-07 12:28:33 +0200

    update

--- src/templates/page/index.js
+++ src/templates/page/index.js
@@ -10,6 +10,13 @@ const Comments = Loadable({
     loader: () => import("../../components/comments"),
     loading: ClipLoader,
 });
+
+
+const SharePage = Loadable({
+    loader: () => import("../../components/share-page"),
+    loading: ClipLoader,
+});
+
 const AddComments = Loadable({
     loader: () => import("../../components/add-comments"),
     loading: ClipLoader,
@@ -17,6 +24,8 @@ const AddComments = Loadable({
 
 
 const Index = (props) => {
+
+    console.log(props , '12312');
     const showSection = (props.data.page.sidebarSettings.sidebarSettings === "Show" ? true : false);
     const [postSettings, setPostSettings] = useState(null);
     useEffect(() => {
@@ -31,6 +40,7 @@ const Index = (props) => {
                 <div className="grid-box">
                     <main>
                         <TheContent text={props?.data?.page?.content} title={props?.data?.page?.title}/>
+                        <SharePage title={props.data.page.title}/>
                     </main>
                     <aside>
                         {showSection === true ? <ReadMore/> : null}
