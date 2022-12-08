f2bce599d910e07271d45c11908052869f94fd6d
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-08 19:11:17 +0200

    update all fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {ThemeProvider} from 'src/context/theme-context';
 
 export const wrapRootElement = ({element}) => {
   return (
-      <ThemeProvider>
         {element}
-      </ThemeProvider>
   )
 };
 
