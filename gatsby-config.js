4702935042c6b1a5a603fe2b15bcead563243ac1
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-08 13:26:29 +0200

    update all fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
       resolve: `gatsby-plugin-sharp`,
       options: {
         defaults: {
@@ -98,11 +109,11 @@ module.exports = {
           requestConcurrency: 1,
           //perPage: 20000,
         },
-        // type: {
-        //   __all: {
-        //     limit: 24,
-        //   },
-        // },
+        type: {
+          __all: {
+            limit: 24,
+          },
+        },
       }
     },
     {
