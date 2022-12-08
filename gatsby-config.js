c6bf0c73ff20c63f0754ef44dbf1e47530b4d443
7FridaysDeveloper <108727315+7FridaysDeveloper@users.noreply.github.com>
2022-12-08 18:59:31 +0200

    update all fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,11 +91,11 @@ module.exports = {
           requestConcurrency: 1,
           //perPage: 20000,
         },
-        type: {
-          __all: {
-            limit: 21,
-          },
-        },
+        // type: {
+        //   __all: {
+        //     limit: 21,
+        //   },
+        // },
       }
     },
     // {
